b7906c65137f46cb0112b48937a46b3c22a9762e
Tactikerl <61546873+Tactikerl@users.noreply.github.com>
2023-06-09 19:09:25 +0200

    bug fix and edit to descriptions of projects

--- src/components/PortfolioPage.jsx
+++ src/components/PortfolioPage.jsx
@@ -16,21 +16,21 @@ const profile = {
 const projects = [
     {
       title: 'Semester Project 2',
-      description: 'This application is developed to emulate a fictional auction house. Users can register with a @stud.noroff email address to create listings with fictional items and bid on listings from other users. Registered users can also check their profile to see acumulated credits, how many listings they have made and how many auctions they have won. non registered users may browse the listings front page and search through them, but have to log in or register an account to access the full app.',
+      description: 'This application is developed to emulate a fictional auction house where users can register with an email address(specifics detailed in repo) to create and bid on listings. Registered users can also check their profile to see accumulated credits, how many listings they have made and how many auctions they have won. non registered users may browse the listings front page and search through them, but have to log in or register an account to access the full app.',
       image: sp2,
       repo: 'https://github.com/Tactikerl/SP2-Noroff-Auction-Application.git',
       liveSite: 'https://tactikerl.github.io/SP2-Noroff-Auction-Application/'
     },
     {
       title: 'JavaScript Frameworks CA',
-      description: 'This is my JavaScript Frameworks CA. In this CA i have created a e-commerce site using React. The site is a fictional site for an unnamed company, and the main focus of the CA is to show my understanding of React and how to use it to create a site for e-commerce.',
+      description: 'This is my JavaScript Frameworks CA. In this CA i have created a e-commerce site using React. The site is a fictional site for an unnamed company, and the main focus of the CA is to show my understanding of React and how to use it to create a site for e-commerce. It is similar to the Semester Project 2, but with a different design and using React instead of vanilla JavaScript.',
       image: jsfca,
       repo: 'https://github.com/Tactikerl/eCommmerce-app-CA.git',
       liveSite: 'https://storied-twilight-7cfba6.netlify.app/'
     },
     {
       title: 'Project Exam 2',
-      description: 'This is my Project Exam for the fourth and last semester of FED(Front-end Development) at Noroff. The exam took 7 weeks and i have tried to create a accommodation booking site following User Stories provided in the exam brief we received.',
+      description: 'This is my Project Exam for the fourth and last semester of FED(Front-end Development) at Noroff. The exam took 7 weeks and i have tried to create a accommodation booking site following User Stories provided in the exam brief we received. The project challenged me to utilize all the skills i have learned during my time at Noroff, and i am very happy with the result. There are some things i would have liked to do differently, but i am overall happy with the result and i feel like i have learned a lot from this project',
       image: pe2,
       repo: 'https://github.com/Tactikerl/pe2-react-app.git',
       liveSite: 'https://pe2-kmk-holidaze.netlify.app/'
@@ -41,11 +41,14 @@ const projects = [
     <a href={href} className="text-blue-500 hover:text-blue-700 underline" target="_blank" rel="noopener noreferrer">{children}</a>
   );
 
-  
+ 
+
   const ProjectCard = ({ image, title, description, repo, liveSite }) => (
     <div className="flex flex-col md:flex-row bg-white rounded-xl shadow-md w-full md:w-2/3 lg:w-2/3 xl:w-2/3 min-h-96 md:min-h-screen/2 m-3">
-      <img className="h-36 w-full object-cover rounded-lg md:w-1/3 md:h-auto" src={image} alt={title} />
-      <div className="p-8 flex flex-col justify-between flex-grow">
+      <div className="w-48 h-48 md:w-64 md:h-64 flex-shrink-0 order-2 md:order-1">
+        <img className="w-full h-full object-cover rounded-lg" src={image} alt={title} />
+      </div>
+      <div className="p-8 flex flex-col justify-between flex-grow order-1 md:order-2">
         <div className="mb-4">
           <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{title}</div>
           <p className="mt-2 text-gray-500">{description}</p>
@@ -62,9 +65,6 @@ const projects = [
       </div>
     </div>
   );
-
- 
-
   
   const ProfileCard = ({ image, name, description, email, github }) => (
     <div className="flex flex-col md:flex-row bg-white rounded-xl shadow-md  max-w-sm md:max-w-md mx-auto md:mx-0 md:min-h-screen/4 m-3">
@@ -83,7 +83,7 @@ const projects = [
   );
 
 const PortfolioPage = () => (
-  <div className="p-6 flex flex-col items-center bg-purple-950">
+  <div className="p-6 flex flex-col items-center bg-purple-950 min-h-screen">
     <ProfileCard {...profile} />
     {projects.map((project) => (
       <ProjectCard key={project.repo} {...project} />
